2b7ab2e628026ef932c5f8c7127f16536483b688
Rylan Meilutis <rylan.meilutis@gmail.com>
2025-11-14 16:40:56 -0500

    fix the pyi

--- python-files/sedsprintf_rs_2026/__init__.py
+++ python-files/sedsprintf_rs_2026/__init__.py
@@ -1,5 +1,5 @@
-from .sedsprintf_rs import *
+from .sedsprintf_rs_2026 import *
 
-__doc__ = sedsprintf_rs.__doc__
-if hasattr(sedsprintf_rs, "__all__"):
-    __all__ = sedsprintf_rs.__all__
+__doc__ = sedsprintf_rs_2026.__doc__
+if hasattr(sedsprintf_rs_2026, "__all__"):
+    __all__ = sedsprintf_rs_2026.__all__
